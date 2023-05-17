3fdc8cd02ed4a5e8a4fadb9224327af451cee54a
Tran Hoang Long <tranhoanglong720@gmail.com>
2023-05-18 01:26:23 +0700

    Role

--- src/component/listrole/ListRole.tsx
+++ src/component/listrole/ListRole.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import styles from "./ListRole.module.scss";
 import classNames from "classnames/bind";
 import Search from "antd/es/input/Search";
 import TableRole from "../table/tablerole/TableRole";
-
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../reudux/hook";
+import { ListRoleFind } from "../reudux/selector";
+import { getListRole } from "../reudux/slices/RoleSlices";
+import { AppContext } from "../context/AppProvider";
 const cx = classNames.bind(styles);
 
 const ListRole = () => {
   const navigate = useNavigate();
+
   const handleChangePageAddRule = () => {
     navigate(
       "/managerruleadd?name=Vai Trò&list=Quản lý vai trò&model=Thêm vai trò"
     );
   };
+  const dispatch = useAppDispatch();
+  const ListRoles = useSelector(ListRoleFind);
+  // const [listUser, setListUser] = useState([]);
+  console.log(ListRoles);
+  useEffect(() => {
+    dispatch(getListRole());
+    // setListUser(ListUser);
+  }, []);
+
   return (
     <Container>
       <div className={cx("wrapListDevice")}>
@@ -25,7 +39,7 @@ const ListRole = () => {
             <Search />
           </div>
         </div>
-        <TableRole />
+        <TableRole data={ListRoles} />
         <div className={cx("wrapAdd")} onClick={handleChangePageAddRule}>
           <img
             src={require("../../assent/add-square.png")}
